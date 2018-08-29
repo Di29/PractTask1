6d9f949dfc770acffa500fbd8f99446cf2be7a41
Уринбасаров Диас Нуржанулы <@corp.itstep>
2018-08-29 17:13:42 +0600

    first push

---
+++ PractTask1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+            Console.WriteLine("Urinbasarov Dias");
+
+            //Task2
+            int number1;
+            int number2;
+            int sum;
+
+            Console.WriteLine("Введите 1-ое число");
+            number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите 2-ое число");
+            number2 = int.Parse(Console.ReadLine());
+
+            sum = number1 + number2;
+
+            Console.WriteLine("Сумма равна: {0}", sum);
+
+            Console.ReadLine();
+        }
+    }
+}
